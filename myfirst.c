1324f64e0f3c6e0474658ee34a27e53705d3aebb
anneshac-2002 <79010534+anneshac-2002@users.noreply.github.com>
2021-02-13 19:19:49 +0530

    Add files via upload

---
+++ myfirst.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a ,b ,c ;
+
+    printf("enter numbers a ");
+    scanf("%d",&a);
+    printf("enter numbers b ");
+    scanf("%d",&b);
+     c= a + b ;
+    printf("sum is %d ", c );
+    return 0;
+
+
+}
+
